7e258aa6b90e554fb0f11afcf2bbce6c4ba7ecc6
Ikramullah <ikramullahzx@gmail.com>
2023-10-24 12:42:14 +0800

    feat: container on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import '$lib/app.css'
 </script>
 
-<div class="w-screen h-screen bg-nord-snow-1">
+<svelte:head>
+  <link href='https://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
+</svelte:head>
+
+<div class="md:container md:mx-auto md:w-1/2 p-3">
   <slot />
 </div>
